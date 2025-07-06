c7c4f58b22c3d6379f1df6c5cf6db97c43995181
Danil <danilbystrov8@gmail.com>
2025-07-06 14:36:20 +0300

    add dynamical information on products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 // import viteLogo from '/vite.svg'
 import './App.module.css';
-import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import { Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
 
 const fetchProductList = () => [
 	{ id: 1, name: 'Телевизор' },
 	{ id: 2, name: 'Компьютер' },
 	{ id: 3, name: 'Телефон' },
 ];
+
+const fetchProduct = (id) =>
+	({
+		1: { id: 1, name: 'Телевизор', price: 29999, amount: 20 },
+		2: { id: 2, name: 'Компьютер', price: 57500, amount: 3 },
+		3: { id: 3, name: 'Телефон', price: 10900, amount: 106 },
+	})[id];
 
 const MainPage = () => (
 	<div>
@@ -19,19 +26,27 @@ const Catalog = () => (
 		<ul>
 			{fetchProductList().map(({ id, name }) => (
 				<li key={id}>
-					<Link to="product">{name}</Link>
+					<Link to={`product/${id}`}>{name}</Link>
 				</li>
 			))}
 		</ul>
 		<Outlet />
 	</div>
 );
-const Product = () => (
-	<div>
-		<h2>Название продукта</h2>
-		<p>Описание продукта</p>
-	</div>
-);
+const Product = () => {
+	const params = useParams()
+
+	const { name, price, amount } = fetchProduct(params.id);
+
+	return (
+		<div>
+			<h2>Товар- {name}</h2>
+			<div>Цена товара: {price}</div>
+			<div>Осталось на складе: {amount}</div>
+			<p>Описание продукта</p>
+		</div>
+	);
+};
 const Contacts = () => (
 	<div>
 		<h1>Наши контакты</h1>
@@ -58,7 +73,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<MainPage />} />
 				<Route path="/catalog" element={<Catalog />}>
-					<Route path="product" element={<Product />} />
+					<Route path="product/:id" element={<Product />} />
 				</Route>
 				<Route path="/contacts" element={<Contacts />} />
 			</Routes>
